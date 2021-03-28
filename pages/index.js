8766d1a0f0cfc74ee14b64136c642f7e74840a32
Rai Velasquez Fermin <raivelasquezf@gmail.com>
2021-03-28 16:41:38 -0500

    Vista principal inicial

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+const page = () => (
+    <>
+        <div className="first">
+            Seccion 1
+        </div>
+        <div className="second">
+            Seccion 2
+        </div>
+        <div className="third">
+            Seccion 3
+        </div>
+
+    </>
+)
+
+export default page
