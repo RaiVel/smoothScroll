af4d772063dccc61b19959d4db49dbc1dd5bbbf0
Rai Velasquez Fermin <raivelasquezf@gmail.com>
2021-03-29 10:07:10 -0500

    Cambios CSS

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 const page = () => (
-    <>
-        <div className="first">
+    <div className="content">
+        <div className ="navbar">
+            <ul>
+                <li>
+                    <a href="#first" as="/">1ro</a>
+                </li>
+                <li>
+                    <a href="#second" as="/">2do</a>
+                </li>
+                <li>
+                    <a href="#third" as="/">3ro</a>
+                </li>
+            </ul>
+        </div>
+        <div className="sites first" id="first">
             Seccion 1
+            <a href="#" as="/">Top</a>
         </div>
-        <div className="second">
+        <div className="sites second" id="second">
             Seccion 2
+            <a href="#" as="/">Top</a>
         </div>
-        <div className="third">
+        <div className="sites third" id="third">
             Seccion 3
+            <a href="#" as="/">Top</a>
         </div>
 
-    </>
+    </div>
 )
 
 export default page
